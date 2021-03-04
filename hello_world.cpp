4611cba0786e284579b3ec8e698863c8ca48efef
morsiiik <ognezvezd02@mail.ru>
2021-03-04 18:31:51 +0300

    clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+<<<<<<< HEAD
 //added comments and changed language
 int main() {
 	std::string name;
 	std::cin>>name;
 	std::cout<<"hello world from "<<name;
+=======
+//добавил комментарии
+int main()
+{
+  std::string name;
+  std::cin >> name;
+  std::cout << "hello world from " << name;
+>>>>>>> clang-format
 }
+
